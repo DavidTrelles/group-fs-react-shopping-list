cd03e39628d1a412cf9483d7cb36aa2278b2dcd5
Krys Frost <librarykrystal@gmail.com>
2023-01-09 19:03:41 -0600

    Files setup, port updated.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 5001;
+const listRouter = require('./routes/list.router.js');
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
@@ -10,6 +10,7 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
+app.use('/list', listRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
